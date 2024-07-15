de337c54c599524b7f4cda96f41a36ccff9a83b3
Umesh Gurjar <95861754+Umyagurjar@users.noreply.github.com>
2024-07-15 11:19:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from os import environ
+
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+ADMIN = int(environ.get("ADMIN", ""))          
+CAPTION = environ.get("CAPTION", "")
+
+# for thumbnail ( back end is UG brain 😉)
+DOWNLOAD_LOCATION = "./DOWNLOADS"
